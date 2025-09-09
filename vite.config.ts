b2745e4f2216317fd4d654f0acd160d4d4629a64
Praveen Tak <praveentak715@gmail.com>
2025-09-09 23:19:26 +0530

    portfolio

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/PraveenTak_Portfolio/',  // <-- Add this line for GitHub Pages subpath
   server: {
     host: "::",
     port: 8080,
